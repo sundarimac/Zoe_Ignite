6a5535b938119335b0bba3d83126e0279b6c72a2
Sundar <sundarimac@gmail.com>
2022-02-27 12:10:29 +0530

    push the latest

--- add.py
+++ add.py
@@ -5,5 +5,6 @@ a =1;
 b = 2;
 c= 40 ;
 print (a + b + c); #same line code edit _ add both changes 
-# some neew line aaded in git 
-variable control 
+# some neew line aaded in git
+#variable control 
+#  #kinder
